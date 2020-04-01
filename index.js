9b395518a5632bc5b72ac18eb54b85a621a46040
Dan Trocchio <dan@golightstream.com>
2020-04-01 13:06:27 -0500

    Remove es6-shim

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 var constants = require('constants');
-var minimumTLSVersion = require('minimum-tls-version');
 
-require('es6-shim');
+var TLS_VERSIONS = [
+	{
+		"name": "tlsv12",
+		"option": constants.SSL_OP_NO_TLSv1_2
+	},
+	{
+		"name": "tlsv11",
+		"option": constants.SSL_OP_NO_TLSv1_1
+	},
+	{
+		"name": "tlsv1",
+		"option": constants.SSL_OP_NO_TLSv1
+	},
+	{
+		"name": "sslv3",
+		"option": constants.SSL_OP_NO_SSLv3
+	},
+	{
+		"name": "sslv2",
+		"option": constants.SSL_OP_NO_SSLv2
+	}
+]
+
+function minimumTLSVersion(tlsVersionName){
+	var index = TLS_VERSIONS.findIndex(function(tlsVersion){
+		return ( tlsVersion.name === tlsVersionName )
+	})
+	if ( index === -1 ) {
+		throw new Error('Invalid TLS version', tlsVersionName);
+	}
+	var relevant = TLS_VERSIONS.slice(index + 1) // +1 because we want to exclude older versions.
+	var minimumTLSVersionValue = 0;
+	// '|'' is a bitwise or
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#.7c_%28Bitwise_OR%29
+	relevant.forEach(function (tlsVersion) {
+		minimumTLSVersionValue = minimumTLSVersionValue | tlsVersion.option;
+	});
+	return minimumTLSVersionValue
+}
+
 var log = console.log.bind(console);
 
 // This object should be free of node-specific logic on order to be kept up to date with Mozilla
